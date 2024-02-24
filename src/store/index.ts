a1f948c6a3e858d104b68204a0468202be13727f
aoshisen <aoshisen.23@gmail.com>
2024-02-24 21:44:32 +0800

    refactor: 文件目录调整 以及const 目录添加

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,26 +3,16 @@ import createBearSlice, { BearSlice } from "./bear";
 import createCounterSlice, { CounterSlice } from "./counter";
 import { persist, devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
+import { STORE_NAME } from "@/assets/const";
 
 export type BoundSlice = BearSlice & CounterSlice;
 
-const createStore = () => {
-  const combinedSlices: StateCreator<BoundSlice> = (...argument) => {
-    const bearSlice = createBearSlice.apply(this, argument);
-    const counterSlice = createCounterSlice.apply(this, argument);
-    return {
-      ...bearSlice,
-      ...counterSlice,
-    };
-  };
-
-  const enhanceStore = devtools(
-    immer(persist(combinedSlices, { name: "bound" }))
-  );
-
-  return create(enhanceStore);
+const useStore = () => {
+  const combinedSlices: StateCreator<BoundSlice> = (...argument) => ({
+    ...createBearSlice.apply(this, argument),
+    ...createCounterSlice.apply(this, argument),
+  });
+  return create(devtools(immer(persist(combinedSlices, { name: STORE_NAME }))));
 };
-
-const useStore = createStore();
 
 export default useStore;
